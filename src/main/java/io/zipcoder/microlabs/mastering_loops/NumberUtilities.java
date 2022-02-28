5b44662ef14cff1695cf1958131134bca7f40f13
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-02-28 15:45:41 -0500

    evenNodd

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,12 +3,49 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        int arraylength = stop - start + 1;
+        int[] array = new int[stop+5];
+        String output = "";
+
+        //build array
+        for(int i = start; i <= stop; i++){
+            array[i] = i;
+            System.out.println(array[i]);
+        }
+
+
+        //check array
+        for (int i = start; i <= stop; i++ )
+        if (array[i]%2 == 0){
+            output += String.valueOf(array[i]);
+            System.out.println(output);
+        }
+        return output;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        int arraylength = stop - start + 1;
+        int[] array = new int[stop+5];
+        String output = "";
+
+        //build array
+        for(int i = start; i <= stop; i++){
+            array[i] = i;
+            System.out.println(array[i]);
+        }
+
+
+        //check array
+        for (int i = start; i <= stop; i++ )
+            if (array[i]%2 == 1){
+                output += String.valueOf(array[i]);
+                System.out.println(output);
+            }
+        return output;
+
     }
 
 

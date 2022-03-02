76fb3fb1cdb5051efccd4d4e4035badfde741840
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-01 19:58:39 -0500

    complete

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -2,19 +2,33 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TriangleUtilities {
 
-    public static String getTriangle(int numberOfRows) {
-        return null;
+    public static String getRow(int numberOfRows) {        //
+
+        String star = "";
+        for( int i = 1; i <= numberOfRows; i= i+1){
+            star+= "*";
+        }
+
+
+        return star;
     }
 
-    public static String getRow(int numberOfStars) {
-        return null;
+    public static String getTriangle(int numberOfStars) {
+
+        String star ="";
+
+        for(int i =1; i <= numberOfStars; i++) {
+            star+= getRow(i) + "\n";
+        }
+
+        return star;
     }
 
-    public static String getSmallTriangle() {
-        return null;
+    public static String getSmallTriangle(int numberOfStars) {
+        return getTriangle(numberOfStars);
     }
 
-    public static String getLargeTriangle() {
-        return null;
+    public static String getLargeTriangle(int numberOfStars) {
+        return getTriangle(numberOfStars);
     }
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilitiesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilitiesTest.java
@@ -27,7 +27,7 @@ public class TriangleUtilitiesTest {
                         "*******\n" +
                         "********\n" +
                         "*********\n";
-        String actual = TriangleUtilities.getTriangle(10);
+        String actual = TriangleUtilities.getTriangle(9);
         Assert.assertEquals(expected, actual);
     }
 
@@ -38,7 +38,7 @@ public class TriangleUtilitiesTest {
                         "**\n" +
                         "***\n" +
                         "****\n";
-        String actual = TriangleUtilities.getTriangle(5);
+        String actual = TriangleUtilities.getTriangle(4);
         Assert.assertEquals(expected, actual);
     }
 
@@ -54,7 +54,7 @@ public class TriangleUtilitiesTest {
                 "*******\n" +
                 "********\n" +
                 "*********\n";
-        String actual = TriangleUtilities.getLargeTriangle();
+        String actual = TriangleUtilities.getLargeTriangle(9);
         Assert.assertEquals(expected, actual);
     }
 
@@ -66,7 +66,7 @@ public class TriangleUtilitiesTest {
                         "**\n" +
                         "***\n" +
                         "****\n";
-        String actual = TriangleUtilities.getSmallTriangle();
+        String actual = TriangleUtilities.getSmallTriangle(4);
         Assert.assertEquals(expected, actual);
     }
 }

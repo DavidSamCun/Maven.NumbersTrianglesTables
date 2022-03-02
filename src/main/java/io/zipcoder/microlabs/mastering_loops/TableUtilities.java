588acb9c9e16590f530fe5ca69d5d5dc13a1f122
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-01 19:18:40 -0500

    did multiply

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,8 +1,12 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
-        return null;
+    public static String getSmallMultiplicationTable(int n) {
+
+
+        return printTable(n);
+
+        //Want result to be "  # |" for each entry into the array
     }
 
     public static String getLargeMultiplicationTable() {
@@ -10,6 +14,57 @@ public class TableUtilities {
     }
 
     public static String getMultiplicationTable(int tableSize) {
+
+        //String output = printRow(tableSize);
+
         return null;
+
+    }
+
+    public static String printRow(int size, int column){
+
+        String output = "";
+        int i = 1;
+        while (i <= column){
+            //System.out.printf("  %4d|", size * i);
+            if (i == column) {
+                output += String.format("%3d |\n",size * i);
+            }//Had a \n after 4d|
+            else {
+                output += String.format("%3d |",size * i);
+            }
+            i++;
+        }
+        System.out.println();
+        return output;
+    }
+
+    public static String printTable(int size){
+
+        String output = "";
+        int i = 1;
+        while (i <= size ) {
+            output += printRow(i , size);
+            i++;
+        }
+        return output;
+    }
+
+    //DEMO from Dolio
+
+/*    public class avg(int... values){
+        int sum = 0;
+        for(int value : values){
+            sum += value;
+        }
+        return sum/values.length;
+        doStuff("I", "Do", "a", "thing"}*/
+
+    public static void doStuff(String... vars){
+        for (String val: vars)
+        {
+            System.out.println(val);
+        }
+
     }
 }
